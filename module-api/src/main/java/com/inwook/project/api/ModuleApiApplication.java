a5c54a6707f42085159e2a1385c145f077b6444f
inwookjung <inwook.jung@kr.doubledown.com>
2023-02-01 00:08:28 +0900

    수정입니다.4

--- module-api/src/main/java/com/inwook/project/api/ModuleApiApplication.java
+++ module-api/src/main/java/com/inwook/project/api/ModuleApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ModuleApiApplication {
-	//수정3
+	//수정4
 	public static void main(String[] args) {
 		SpringApplication.run(ModuleApiApplication.class, args);
 	}
