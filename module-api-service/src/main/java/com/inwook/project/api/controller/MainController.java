a2f3d40d3f3e95e282d79e3dec22b4fd9a0c87f7
inwookjung <inwook.jung@kr.doubledown.com>
2023-02-07 01:05:23 +0900

    module-api-service mvc적용

---
+++ module-api-service/src/main/java/com/inwook/project/api/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.inwook.project.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@GetMapping("/")
+	public String get() {
+		return "success";
+	}
+}
--- module-common/src/main/java/com/inwook/project/common/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.inwook.project.common;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
